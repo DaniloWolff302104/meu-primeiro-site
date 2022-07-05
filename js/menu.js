111afa5c2c3fac32b05eb7babc4a67650e4345b7
Danilo <danilogwolff@hotmail.com>
2022-07-05 15:01:14 -0300

    Primeiro Comit - Primeiro site ( projeto )

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+
+
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+
+btnMenu.addEventListener('click', function(){
+	
+	nav.classList.toggle('mostrar');
+});
